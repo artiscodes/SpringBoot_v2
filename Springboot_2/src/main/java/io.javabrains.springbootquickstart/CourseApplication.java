9f96bf4efedadead0048dc755e879ba85a45f0c1
Art <artiscodes@gmail.com>
2018-08-03 17:04:30 +0200

    firat commit

---
+++ Springboot_2/src/main/java/io.javabrains.springbootquickstart/CourseApplication.java
@@ -0,0 +1,12 @@
+package Springboot_2.Springboot_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+}
